18555461bef00a32a2a4dbef9daaa238589b4e44
Aleksander Gorecki <aleksandergorecki80@gmail.com>
2018-05-29 00:18:41 +0100

    add counters names

--- components/CounterOne.js
+++ components/CounterOne.js
@@ -3,10 +3,43 @@ class CounterOne extends React.Component {
     constructor() {
         super();
         this.state = {
-            counterName: "Counter one",
-            counterState: 0,
+            counterState: 0
         };
       }
+
+          /*      life stages     */
+    componentWillMount(){   // działa
+      console.log("Component will mount");
+    }
+
+    componentDidMount(){  // działa
+      console.log("Component did mount ")
+    }
+
+    componentWillReceiveProps(nextProps){
+      console.log("Component will receive props", nextProps)
+    }
+
+
+    shouldComponentUpdate(nextProps, nextState) { // działa
+      console.log(" Should component update", nextProps, nextState);
+      
+      return true;
+    }
+
+    componentWillUpdate(nextProps, nextState){ // działa
+      console.log("Component will update ", nextState, nextProps);
+    }
+
+    componentDidUpdate(prevProps, prevState){ // działa
+    console.log('Component did update', prevProps, prevState);       
+    }
+
+    componentWillUnmount(){
+      console.log("Component will unmount");
+    }
+
+/*       ***********      */
 
     increaseState(){
       this.setState({
@@ -20,16 +53,28 @@ class CounterOne extends React.Component {
       });
     }
 
+
+
  render() {
   return (
    <div >
       <p>
         <button className = "btn" onClick = {this.decreaseState.bind(this)}>-</button>
-        <span>{this.state.counterName}</span>
+        <span>{this.props.counterId} . {this.props.counterName}</span>
         <span className="badge">{this.state.counterState}</span>
         <button className = "btn" onClick = {this.increaseState.bind(this)}>+</button>
+        <input type="text" />
       </p>
    </div>
   );
  }
 }
+
+
+CounterOne.propTypes = {
+  counterName: React.PropTypes.string,
+  counterId: React.PropTypes.number
+};
+
+
+
--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 class App extends React.Component {
+
+
+
+	onCangeName(newName){
+
+	}
+
 	render(){
+
+		var counters = [
+			{
+			id: 1,
+			name:  "Counter one"},
+			{
+			id: 2,
+			name:  "Counter two"},
+			{
+			id: 3,
+			name:  "Counter three"},
+		];
+
+console.log(counters[0].name);
 		return (
 			<div className="container">
-				<CounterOne />
-				<CounterOne />
-				<CounterOne />
+				<CounterOne counterName = {counters[0].name} counterId={counters[0].id}/>
+				<CounterOne counterName = {counters[1].name} counterId={counters[1].id}/>
+				<CounterOne counterName = {counters[2].name} counterId={counters[2].id}/>
+
 			</div>
 		);
 	}
