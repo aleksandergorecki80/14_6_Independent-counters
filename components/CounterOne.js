91e249bcfe4b5e7626d5c7f035e0f965ccb05da6
Aleksander Gorecki <aleksandergorecki80@gmail.com>
2018-06-01 17:30:07 +0100

    sending for class CounterOne extends React.Component {

--- components/CounterOne.js
+++ components/CounterOne.js
@@ -1,42 +1,41 @@
 class CounterOne extends React.Component {
- constructor() {
-  super();
-  this.state = {
-   counter: 0
-  };
-  this.kki = 'kki';
- }
+  constructor() {
+    super();
+    this.state = {
+      counter: 0
+    };
+  }
 
- increaseState() {
-  this.setState(prevState => ({
- counter: prevState.counter + 1
-}));
- }
+  increaseState() {
+    this.setState(prevState => ({
+      counter: prevState.counter + 1
+    }));
+  }
 
- decreaseState() {
-  this.setState(prevState => ({
- counter: prevState.counter - 1
-}));
- }
+  decreaseState() {
+    this.setState(prevState => ({
+      counter: prevState.counter - 1
+    }));
+  }
 
- render() {
-  return (
-   <div className="counter">
-    <p>{this.props.counterName}</p>
-    <button className="btn" onClick={this.decreaseState.bind(this)}>
-     -
-    </button>
-    <span className="badge">{this.state.counter}</span>
-    <button className="btn" onClick={this.increaseState.bind(this)}>
-     + {this.kki}
-    </button>
-    <hr />
-    <LifeStages componentName="CounterOne" />
-   </div>
-  );
- }
+  render() {
+    return (
+      <div className="counter">
+        <p>{this.props.counterName}</p>
+        <button className="btn" onClick={this.decreaseState.bind(this)}>
+          -
+        </button>
+        <span className="badge">{this.state.counter}</span>
+        <button className="btn" onClick={this.increaseState.bind(this)}>
+          +
+        </button>
+        <hr />
+        <LifeStages componentName="CounterOne" />
+      </div>
+    );
+  }
 }
 
 CounterOne.propTypes = {
- counterName: React.PropTypes.string
+  counterName: React.PropTypes.string
 };
